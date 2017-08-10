d2ffb19e2ef7ae38c8c5b78bca31de7abd29686f
askalerion <bednartd@rose-hulman.edu>
2017-08-10 18:39:19 -0400

    Added anuglar firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
 import { MyApp } from './app.component';
 import { QuoteDetailPageModule } from "../pages/quote-detail/quote-detail.module";
 import { ListPageModule } from "../pages/list/list.module";
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCFIsK-89kcIEUB-cxxRL8sth3UhWIaxdQ",
+  authDomain: "bednartd-ionic-movie-quotes.firebaseapp.com",
+  databaseURL: "https://bednartd-ionic-movie-quotes.firebaseio.com",
+  projectId: "bednartd-ionic-movie-quotes",
+  storageBucket: "bednartd-ionic-movie-quotes.appspot.com",
+  messagingSenderId: "194019125509"
+}
 
 @NgModule({
   declarations: [
@@ -17,6 +27,8 @@ import { ListPageModule } from "../pages/list/list.module";
     IonicModule.forRoot(MyApp),
     QuoteDetailPageModule,
     ListPageModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
