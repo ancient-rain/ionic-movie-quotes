75d93da08ebd9ba653db19df41b0fedfd9cbe96f
askalerion <bednartd@rose-hulman.edu>
2017-08-10 18:13:24 -0400

    Removed home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 import { QuoteDetailPageModule } from "../pages/quote-detail/quote-detail.module";
 import { ListPage } from "../pages/list/list";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +21,6 @@ import { ListPage } from "../pages/list/list";
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
